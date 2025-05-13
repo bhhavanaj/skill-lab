dbb002c15ee89ba326a4d5147997a4a3f4ed0dbf
bhhavanaj <bhavana0322@gmail.com>
2025-05-13 15:40:53 +0530

    siet.c

---
+++ siet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int siet()
+{
+    printf("BHAVANA\n");
+    printf("03:12:2005\n");
+    printf("19\n");
+    printf("9731810437\n");
+}
+int main()
+{
+    siet();
+    return 0;
+}
+
