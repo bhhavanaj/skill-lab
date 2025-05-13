c70175c39bc3e0317ee3c341354709f00f310c7d
bhhavanaj <bhavana0322@gmail.com>
2025-05-13 10:52:17 +0530

    student info.c

---
+++ student info.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char name[] ="BHAVANA";
+    char location[] ="CHIKKAMAGALUR";
+    int age = 19;
+    printf("Name:%s\n",name);
+    printf("Age:%d\n",age);
+    printf("Location:%s\n",location);
+    return 0;
+}
+
+output
+Name:BHAVANA
+Age:19
+Location:CHIKKAMAGALUR
