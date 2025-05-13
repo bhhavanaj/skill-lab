4362476ca4cee68f9074438ed7741332ca62e07e
bhhavanaj <bhavana0322@gmail.com>
2025-05-13 11:13:13 +0530

    2interger.c

---
+++ 2interger.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num1,num2,sum;
+    printf("enter the first interger:");
+    scanf("%d",&num1);
+    printf("enter the second interger:");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("sum of %d and %d is %d",num1,num2,sum);
+    return 0;
+}
