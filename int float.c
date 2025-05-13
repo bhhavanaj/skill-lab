3c91ad6bf2dfc6bc5c9e0c2546e9cbf4a7d3ddbc
bhhavanaj <bhavana0322@gmail.com>
2025-05-13 11:18:19 +0530

    int float.c

---
+++ int float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int intvalue;
+    float floatvalue;
+    printf("Enter an interger:");
+    scanf("%d",&intvalue);
+    printf("enter the float:");
+    scanf("%d",&floatvalue);
+    printf("you entered the integer:%d\n",intvalue);
+    printf("you entered the float:%d\n",floatvalue);
+    return 0;
+}
