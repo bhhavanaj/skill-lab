71b7f9aea1b521e5ef8bc6b1c1d833047ebddb03
bhhavanaj <bhavana0322@gmail.com>
2025-05-13 10:48:16 +0530

    positive num.c

---
+++ positive num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    if(num>0)
+    {
+        printf("The entered number is positive");
+    }
+    return 0;
+}
+
+output
+Enter the number:44
+The entered number is positive
