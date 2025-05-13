ef4d79f338fcd275a73f88c5929d2577b60ebc93
bhhavanaj <bhavana0322@gmail.com>
2025-05-13 10:06:20 +0530

    simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    float p,r,t,si;
+    printf("enter the value of p,r and t ");
+    scanf("%f%f%f",&p,&r,&t);
+    si=(p*t*r)/100;
+    printf("result is %f",si);
+}
+output
+enter the value of p,r and t 
+20
+3
+10
+result is 6.000000
