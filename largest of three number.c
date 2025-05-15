8ab3fcb0d6fa41f7c023a69f590b28efe1eca827
bhhavanaj <bhavana0322@gmail.com>
2025-05-15 09:43:56 +0530

    largest of three number.c

---
+++ largest of three number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int a,b,c;
+int large()
+{
+    if(a>b&&a>c)
+    return a;
+    else if(b>a&&b>c)
+    return b;
+    else
+    return c;
+}
+int main()
+{
+    printf("enter the value of a,b,c");
+    scanf("%d%d%d",&a,&b,&c);
+    int result=large(a,b,c);
+    printf("%d",result);
+    return 0;
+}
