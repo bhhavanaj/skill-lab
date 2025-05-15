8b5b4a2978765165793c0816e070c829a3ba294f
bhhavanaj <bhavana0322@gmail.com>
2025-05-15 21:06:07 +0530

    string to function.c

---
+++ string to function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int i,n;
+int each(char arr[n])
+{
+    for(i=0;i<n;i++)
+    {
+        printf("%c",arr[i]);
+    }
+}
+int main()
+{
+    char arr[]={"bhavana"};
+    n=strlen(arr);
+    int res=each(arr);
+    return 0;
+}
